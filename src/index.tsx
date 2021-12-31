edb88981a113186a918c42a1ce5e48b1097ab78c
juniorsilvaeng <juniorsilvacc@hotmail.com>
2021-12-31 11:14:11 -0300

    axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {createServer} from 'miragejs';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,4 +29,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-reportWebVitals();
+
